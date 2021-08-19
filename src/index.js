c1dda5e52d29faa561139799a66d2174628f1a05
hanul-cha <ckgks3014@naver.com>
2021-08-19 12:10:04 +0900

    action

--- src/index.js
+++ src/index.js
@@ -4,12 +4,26 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-  
-  return count;
+const countModifier = (count = 0, action) => {//reducer라고 불림 함수의 형태여야함, 여기서 데이터 형태를 변환시킴
+  if (action.type === 'ADD') {
+    return count + 1;
+  } else if (action.type === 'MINUS') {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
-const countStore = createStore(countModifier); //state를 저장하는 저장소가 될것임
+const countStore = createStore(countModifier); //state를 저장하는 저장소가 될것임, reducer를 호출해줘야함
+
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'MINUS' });
+
+console.log(countStore.getState());
 
 
 
