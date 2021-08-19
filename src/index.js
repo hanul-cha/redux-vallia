bfc6fd406dbf90580ba63b637d5913f27e52414a
hanul-cha <ckgks3014@naver.com>
2021-08-19 12:47:16 +0900

    end basic

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {//reducer라고 불림 함수의 형태여야함, 여기서 데이터 형태를 변환시킴
-  if (action.type === 'ADD') {
-    return count + 1;
-  } else if (action.type === 'MINUS') {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -25,11 +29,11 @@ const onChange = () => {
 countStore.subscribe(onChange); //data에 변화가 있을때마다 호출해줄것임
 
 const handleAdd = () => {
-  countStore.dispatch({ type:"ADD" })//액션을 디스페치로 연결해주었음
+  countStore.dispatch({ type:ADD })//액션을 디스페치로 연결해주었음
 }
 
 const handleMinus = () => {
-  countStore.dispatch({ type:"MINUS" })
+  countStore.dispatch({ type:MINUS })
 }
 
 add.addEventListener('click', handleAdd);
