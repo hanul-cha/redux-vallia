902fbca833045dfd38bfd397a48128f1ebf9150f
hanul-cha <ckgks3014@naver.com>
2021-08-21 15:09:32 +0900

    How to dispatch from another file

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-function Home({ toDos }) {
+function Home({ toDos, addToDo }) {//toDos에 할당된것은 mapStateToProps에서 가져온 reducer의 state이다
     
     const [text, setText] = useState("");
 
@@ -12,6 +13,7 @@ function Home({ toDos }) {
     function onSubmit(e) {
         e.preventDefault();
         console.log(text);
+        addToDo(text);
         setText("");
     }
 
@@ -27,9 +29,19 @@ function Home({ toDos }) {
     )
 }
 
-function mapStateToProps(state) {
+function mapStateToProps(state) {//여기서 state는 reducer에 할당되어 있는 state임
     return { toDos: state }; //이부분이 home의 prop으로 추가되었음
 }
 
-export default connect(mapStateToProps) (Home);
+function mapDispatchToProps(dispatch) {
+    return {
+        addToDo: text => dispatch(actionCreators.addToDo(text))
+        //확장된actionCreators.addToDo에 value값인 text를 할당해 리턴된값을 dispatch해줌
+        //그값이 이 함수로 home의 prop으로 사용할수 있게되는 원리다
+        //리턴되어 오는 text에서 필요한 argument는 위에 함수에서 불러올것이다
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (Home);
 //connect()는 home으로 보내는 props에 추가될수 있도록 허용해준다
+//addToDo또한 home의 prop으로 사용할수 있음
